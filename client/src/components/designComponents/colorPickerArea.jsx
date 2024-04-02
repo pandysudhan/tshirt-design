7b7d45507826a095826527e92003a65704384bb9
pandysudhan <pandysudhan@gmail.com>
2024-04-02 10:19:18 -0500

    resizable image feature added

--- client/src/components/designComponents/colorPickerArea.jsx
+++ client/src/components/designComponents/colorPickerArea.jsx
@@ -1,6 +1,4 @@
 export default function colorPickerArea({ data, onDataChange, currView }) {
-  
-
 
   function onColorChange(id, new_color) {
     const updatedColors = data.colors.map((color) => {
@@ -14,10 +12,21 @@ export default function colorPickerArea({ data, onDataChange, currView }) {
   }
 
   function updateImagesForView(img) {
-    const updatedViews = data.views.map(view => {
-      console.log(view);
+    const updatedViews = data.views.map((view) => {
       if (view.view_name === currView) {
         const updatedImages = [...view.images, { img_data: img, position: { x: 0, y: 0 } }];
+        return { ...view, images: updatedImages };
+      }
+      return view;
+    });
+
+    onDataChange({ ...data, views: updatedViews });
+  }
+
+  function removeImageFromView(imgIndex) {
+    const updatedViews = data.views.map((view) => {
+      if (view.view_name === currView) {
+        const updatedImages = view.images.filter((_, index) => index !== imgIndex);
         return { ...view, images: updatedImages };
       }
       return view;
@@ -41,18 +50,12 @@ export default function colorPickerArea({ data, onDataChange, currView }) {
     reader.readAsDataURL(file);
   };
 
-
   return (
     <div className="flex flex-row">
       <div className="flex flex-col">
         {data.colors.map((color) => (
-          <div
-            key={"color_picker_div" + color.id}
-            className="flex flex-row my-3"
-          >
-            <label key={"color_picker_label_" + color.id}>
-              {color.display_name}
-            </label>
+          <div key={"color_picker_div" + color.id} className="flex flex-row my-3">
+            <label key={"color_picker_label_" + color.id}>{color.display_name}</label>
             <input
               key={"color_picker_" + color.id}
               type="color"
@@ -75,6 +78,15 @@ export default function colorPickerArea({ data, onDataChange, currView }) {
         <div id="imgPicker">
           <input type="file" onChange={handleFileChange} />
         </div>
+
+        {data.views.map((view, viewIndex) => (
+          view.view_name === currView && view.images.map((image, imgIndex) => (
+            <div key={`image_${viewIndex}_${imgIndex}`} className="flex flex-row my-3">
+              <img src={image.img_data.src} alt={`Uploaded ${imgIndex + 1}`} style={{ maxWidth: '100px', maxHeight: '100px', marginRight: '10px' }} />
+              <button onClick={() => removeImageFromView(imgIndex)}>Remove</button>
+            </div>
+          ))
+        ))}
       </div>
     </div>
   );
--- client/src/components/designComponents/kanvasArea.jsx
+++ client/src/components/designComponents/kanvasArea.jsx
@@ -2,43 +2,43 @@ import { useEffect, useRef } from "react";
 import Konva from "konva";
 
 export default function KanvasArea({ data, onDataChange, currView }) {
-
   const layerRef = useRef(null);
   const stageRef = useRef(null);
-
-  const currViewData = data.views.find((view) => view.view_name === currView);
   const colorData = data.colors;
 
-  function onImagePositionChange(imgIdx, position) {
-    const currViewData = data.views.find(view => view.view_name === currView);
-    const currViewImages = currViewData.images || [];
-  
+  const currViewData = data.views.find((view) => view.view_name === currView);
+  const currViewImages = currViewData.images || [];
+
+  function onImagePositionChange(imgIdx, position, size) {
+ 
     const updatedImages = currViewImages.map((image, index) => {
       if (index === imgIdx) {
-        return { ...image, position: position };
+        return { ...image, position: position, size: size };
       }
       return image;
     });
-  
+
     const updatedViewData = { ...currViewData, images: updatedImages };
-    const updatedViews = data.views.map(view =>
+    const updatedViews = data.views.map((view) =>
       view.view_name === currView ? updatedViewData : view
     );
-  
+
     const updatedData = { ...data, views: updatedViews };
     onDataChange(updatedData);
-  
+
     console.log("changed position of img " + imgIdx, position);
     console.log(updatedData);
   }
-
   const drawImageOnCanvas = (idx, img) => {
     const layer = layerRef.current;
   
+    const width = img.size ? img.size.width : 80;
+    const height = img.size ? img.size.height : 80;
+  
     const konvaImage = new Konva.Image({
       image: img.img_data,
-      width: 100,
-      height: 100,
+      width: width,
+      height: height,
       x: img.position.x,
       y: img.position.y,
       draggable: true,
@@ -50,7 +50,7 @@ export default function KanvasArea({ data, onDataChange, currView }) {
     });
   
     konvaImage.on("dragend", function () {
-      onImagePositionChange(idx, { x: this.x(), y: this.y() });
+      onImagePositionChange(idx, { x: this.x(), y: this.y() }, { width: this.width(), height: this.height() });
     });
   
     layer.add(konvaImage);
@@ -59,18 +59,32 @@ export default function KanvasArea({ data, onDataChange, currView }) {
     const transformer = new Konva.Transformer({
       nodes: [konvaImage],
       keepRatio: true, // Maintain aspect ratio while resizing
-      boundBoxFunc: function(oldBox, newBox) {
+      boundBoxFunc: function (oldBox, newBox) {
         // Limit resizing to canvas boundaries
         if (newBox.width < 10 || newBox.height < 10) {
           return oldBox;
         }
         return newBox;
-      }
+      },
+      visible: false, // Initially hide transformer
     });
+  
     layer.add(transformer);
+  
+    // Handle click event on the image
+    konvaImage.on('mousedown', (e) => {
+      transformer.visible(!transformer.visible());
+      layer.draw();
+    });
+  
+    transformer.on('transformend', () => {
+      const updatedSize = { width: transformer.width(), height: transformer.height() };
+      onImagePositionChange(idx, { x: konvaImage.x(), y: konvaImage.y() }, updatedSize);
+    });
   
     layer.draw();
   };
+  
   
 
   useEffect(() => {
@@ -85,9 +99,7 @@ export default function KanvasArea({ data, onDataChange, currView }) {
     layerRef.current = layer;
 
     for (const productPart of currViewData.parts) {
-      const fill_color = colorData.find(
-        (item) => item.id === productPart.color
-      );
+      const fill_color = colorData.find((item) => item.id === productPart.color);
 
       const tempPart = new Konva.Line({
         points: productPart.coordinates,
@@ -100,8 +112,7 @@ export default function KanvasArea({ data, onDataChange, currView }) {
       layer.add(tempPart);
     }
 
-
-    const currViewData_ = data.views.find(view => view.view_name === currView);
+    const currViewData_ = data.views.find((view) => view.view_name === currView);
     const currViewImages_ = currViewData_.images || [];
 
     for (const [idx, img] of currViewImages_.entries()) {
